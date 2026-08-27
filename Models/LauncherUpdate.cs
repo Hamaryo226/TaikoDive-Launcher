3ec11fe7a0d87b2fe0fd48f661887d6857bbffac
Ryota Hamaguchi <52881627+Hamaryo226@users.noreply.github.com>
2026-08-28 07:21:38 +0900

    Add launcher update release notes to manifest

--- Models/LauncherUpdate.cs
+++ Models/LauncherUpdate.cs
@@ -15,6 +15,8 @@ public sealed class LauncherUpdateManifest
 {
     public string Revision { get; set; } = string.Empty;
 
+    public string ReleaseNotes { get; set; } = string.Empty;
+
     public string Sha256 { get; set; } = string.Empty;
 
     public long Size { get; set; }
